e7ecc10c7ef22dadcde1162898ec67e51591705b
Carlos J <carlosjb1987@gmail.com>
2022-08-08 20:15:49 +0200

    Ejercicio 2

---
+++ Tema4/Ejer2.py
@@ -0,0 +1,10 @@
+numero_inicial = 3
+numero_final =11
+
+while numero_inicial<numero_final:
+    
+    if numero_inicial % 2 != 0:
+        print (numero_inicial)
+    
+    numero_inicial += 1
+        
