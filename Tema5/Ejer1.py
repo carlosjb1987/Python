409697ea49480ffc3072336d47ead56d79f06159
Carlos J <carlosjb1987@gmail.com>
2022-08-10 16:46:11 +0200

    Tema 5

---
+++ Tema5/Ejer1.py
@@ -0,0 +1,11 @@
+
+def areaTriangulo (altura,base):
+    return (base*altura)/2
+
+print(areaTriangulo(2,1))
+
+def areaCirculo(radio):
+    pi=3.14
+    return pi*(radio**2)
+
+print (areaCirculo(5))
